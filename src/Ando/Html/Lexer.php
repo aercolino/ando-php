ae8157281ec8442a7b7e985a2f0f9eb1a17e2a0f
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2015-01-18 23:19:34 +0200

    Added copyright notice.

--- src/Ando/Html/Lexer.php
+++ src/Ando/Html/Lexer.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link https://github.com/aercolino/ando-php
+ * @copyright Copyright (c) 2015 Andrea Ercolino
+ * @license https://github.com/aercolino/ando-php/blob/master/LICENSE
+ */
 
 class Ando_Html_Lexer
 {
